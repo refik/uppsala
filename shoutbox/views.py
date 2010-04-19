ce66780da9214946034b3682961143e6266c3be8
Yigit Guneli <gro.tiiy@yiit.org>
2010-04-19 22:05:27 +0200

    added gravatar support for shoutbox

--- shoutbox/views.py
+++ shoutbox/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from uppsala.shoutbox.models import shout
+import hashlib
 
 def index(request):
-    latest_shouts_list = shout.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('base_generic.html', {'latest_shouts': latest_shouts_list})
+	latest_shouts_list = shout.objects.all().order_by('-pub_date')[:5]
+	gravatars = []
+	for x in latest_shouts_list:
+		#x.append((hashlib.md5(x.user.email).hexdigest()))
+		x.ahmet = hashlib.md5(x.user.email).hexdigest()
+		gravatars.append(x)
+	#print x.ahmet
+	#print gravatars
+	return render_to_response('base_generic.html', {
+		'latest_shouts': latest_shouts_list,
+		})
 
 def addShout(request):
 	if request.user.is_authenticated():
-		kullanici=request.user
+		kullanici = request.user
 		shouts = shout.objects.all()
-		if len(request.POST['shout'])>0:
+		if len(request.POST['shout']) > 0:
 			try:
 				yazilmis = request.POST['shout']
 			except (KeyError):
@@ -19,10 +29,10 @@ def addShout(request):
 					'error_message': "Please enter a shout",
 				})
 			else:
-				yeniShout = shouts.create(shout=yazilmis,user=kullanici)
+				yeniShout = shouts.create(shout = yazilmis, user = kullanici)
 				return HttpResponseRedirect(reverse('uppsala.shoutbox.views.index'))
 	else:
-		kullanici=False
+		kullanici = False
 		return render_to_response('shoutbox/shout.html',{
 				'error_message': "You need to login.",
 			})
