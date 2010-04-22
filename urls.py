37bc12e23cb016b4467a914583ad5823b9695b23
refik <refik.rfk@gmail.com>
2010-04-22 14:56:20 +0300

    login and registration support

--- urls.py
+++ urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
 	(r'^shouts/', include('uppsala.shoutbox.urls')),
 	(r'^meet/', include('uppsala.meet.urls')),
 	(r'^admin/', include(admin.site.urls)),
+	(r'^users/', include('uppsala.users.urls')),
 	(r'^comments/', include('django.contrib.comments.urls')),
 )
