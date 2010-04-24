36bea0fd687df00c0f5d1ab2950f4feaa7df2c23
Yigit Guneli <gro.tiiy@yiit.org>
2010-04-25 01:52:26 +0200

    adding the fileshare app

---
+++ fileshare/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from uppsala.fileshare.models import UploadedFile
+from uppsala.fileshare.forms import *
+from uppsala.settings import PROJECT_PATH,MEDIA_URL,ADMIN_MEDIA_PREFIX
+
+def index(request):
+	shared_files = UploadedFile.objects.all()
+	form = UploadFileForm()
+	return render_to_response('fileshare/base_generic.html', {
+		'shared_files': shared_files,
+		'form': form,
+		'media_url': MEDIA_URL,
+		})
+
+def handle_uploaded_file(f):
+	file_path = PROJECT_PATH + ADMIN_MEDIA_PREFIX + str(f)
+	destination = open(file_path, 'wb+')
+	for chunk in f.chunks():
+		destination.write(chunk)
+	destination.close()
+	return file_path
+
+    
+def addFile(request):
+	if request.user.is_authenticated():
+		kullanici = request.user
+		shared_files = UploadedFile.objects.all()
+		if request.method == 'POST':
+			form = UploadFileForm(request.POST, request.FILES)
+			
+			if form.is_valid():
+				name = request.FILES['file']
+				path = handle_uploaded_file(name)
+				print request.FILES['file']
+				yeniFile = shared_files.create(user = kullanici, file_path = path, file_name = str(name), is_public = True, share_to = kullanici)
+				return HttpResponseRedirect('/fileshare/')
+			else:
+				print "ahmet"
+				form = UploadFileForm()
+		return render_to_response('fileshare/base_generic.html', {
+			'form': form,
+			})
+	else:
+		kullanici = False
+		return render_to_response('fileshare/base_generic.html',{
+				'error_message': "You need to login.",
+			})
+
+		
--- urls.py
+++ urls.py
@@ -1,24 +1,23 @@
 from django.conf.urls.defaults import *
-
+from uppsala.settings import PROJECT_PATH, DEBUG
 from django.contrib import admin
 admin.autodiscover()
 
+media_path =  PROJECT_PATH+'/media'
+#print media_path
 urlpatterns = patterns('',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2efdd48d83184d8b795bc2fe9bc4cb437e2a5685
 	(r'^shouts/', include('uppsala.shoutbox.urls')),
 	(r'^meet/', include('uppsala.meet.urls')),
 	(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
-=======
-    (r'^shouts/', include('uppsala.shoutbox.urls')),
-    (r'^meet/', include('uppsala.meet.urls')),
-    (r'^admin/', include(admin.site.urls)),
->>>>>>> a338b894e4ed9ccdc497c33e6e34af04859b56a3
-=======
 	(r'^users/', include('uppsala.users.urls')),
+	(r'^fileshare/', include('uppsala.fileshare.urls')),
 	(r'^comments/', include('django.contrib.comments.urls')),
->>>>>>> 2efdd48d83184d8b795bc2fe9bc4cb437e2a5685
+
 )
+
+# Media Folder (for the development phase. Should be deleted after)
+if DEBUG:
+	urlpatterns += patterns('',
+		(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '%s/media' % PROJECT_PATH}),
+	)
+
