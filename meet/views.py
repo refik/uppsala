27e676c846467b8f804c9ded2165c7f68ee3e44f
Yigit Guneli <gro.tiiy@yiit.org>
2010-05-09 16:05:21 +0200

    rendered_with decorator test

--- meet/views.py
+++ meet/views.py
@@ -16,50 +16,47 @@ def index(request):
 @login_required
 @rendered_with("meet/result.html")
 def result(request, meet_id):
-	p = get_object_or_404(Meet, pk=meet_id)
+	meet = get_object_or_404(Meet, pk=meet_id)
 	return locals()
 
 @login_required
 @rendered_with("meet/detail.html")
 def detail(request, meet_id):
-	p = get_object_or_404(Meet, pk=meet_id)
+	meet = get_object_or_404(Meet, pk=meet_id)
 	return locals()
 
 @login_required
+@rendered_with("meet/detail.html")
 def vote(request, meet_id):
-	p = get_object_or_404(Meet, pk=meet_id)
+	meet = get_object_or_404(Meet, pk=meet_id)
 	try:
 		selected_choice = p.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
-		return render_to_response('meet/detail.html', {
-			'meet': p,
-			'error_message': "You didn't select a choice.",
-		}, context_instance=RequestContext(request))
+		error_message = "You didn't select a choice"
+		return locals()		
 	else:
 		selected_choice.vote += 1
 		selected_choice.save()
-	return HttpResponseRedirect(reverse('uppsala.meet.views.result', args=(p.id,)))
+		return locals()
 
-@login_required
+@rendered_with("meet/new.html")
 def new(request):
-	return render_to_response('meet/new.html',{
-			}, context_instance=RequestContext(request))
+	return locals()
 
 @login_required
+@rendered_with("meet/result.html")
 def create(request):
 	form = newMeet(request.POST)
-	if form.is_valid() and request.user.is_authenticated():
+	if form.is_valid()
 		sender_new = request.user
 		date_new = form.cleaned_data['date']
 		place_new = form.cleaned_data['place']    
-		n = Meet(date=date_new, place=place_new, sender=sender_new)
-		n.save()
-		n.choice_set.create(choice='gelirim', vote=0)
-		n.choice_set.create(choice='gunde anlastik', vote=0)
-		n.choice_set.create(choice='emin degilim', vote=0)
-		n.choice_set.create(choice='gelemem', vote=0) 
-		n.save()
-		return HttpResponseRedirect(reverse('uppsala.meet.views.result', args=(n.id,)))
-	else:
-		return render_to_response('meet/new.html',{
-				}, context_instance=RequestContext(request))
+		meet = Meet(date=date_new, place=place_new, sender=sender_new)
+		meet.save()
+		meet.choice_set.create(choice='gelirim', vote=0)
+		meet.choice_set.create(choice='gunde anlastik', vote=0)
+		meet.choice_set.create(choice='emin degilim', vote=0)
+		meet.choice_set.create(choice='gelemem', vote=0) 
+		meet.save()
+		return locals()
+	
